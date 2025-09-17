b2885b3663454df0ac74ba74be278d352962a2c2
chetan8085patel@gmail.com <chetan8085patel@gmail.com>
2025-09-17 11:30:40 +0530

    9/17

---
+++ 10_airthmati.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main()
+{
+    int a, b, c;
+    a = 34;
+    b = 52;
+    c = a % b;
+    printf("ans = %d", c);
+}
---
+++ 11_relational_operator.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main()
+{
+    int a, b, c;
+    a = 18;
+    b = 8;
+    c = a != b;
+    printf("ans = %d", c);
+}
