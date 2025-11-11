f53d589bfb9eeaefa25a4c470b3ecaecfb3bda8a
chetan8085patel@gmail.com <chetan8085patel@gmail.com>
2025-11-11 11:30:18 +0530

    11/05

---
+++ 118_perfactsquare.c
@@ -0,0 +1,33 @@
+// wap to check given number is perfact square or not.
+#include <stdio.h>
+void main()
+{
+    int num, f, i;
+    do
+    {
+        printf("enter a number :");
+        scanf("%d", &num);
+        while (1)
+        {
+            if (i * i == num)
+            {
+                f = 1;
+                break;
+            }
+            if (i * i > num)
+            {
+                f = 0;
+                break;
+            }
+            i++;
+        }
+        if (f == 1)
+        {
+            printf("num is perfact square of %d \n", i);
+        }
+        else
+        {
+            printf("num is not perfact square\n");
+        }
+    } while (f == 0);
+}
